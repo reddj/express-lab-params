683b3ba7eb563d1ea1dfe9e6ddd1c29734267c9a
Johnathan <reddjohnathan@gmail.com>
2019-01-15 15:29:00 -0600

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+let question = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
 let express = require('express');
 let app = express();
@@ -17,5 +18,16 @@ app.get('/tip/:total/:tipPercentage', (request, response) => {
 function calculateTip(total, tipPercentage){
 	return total * (tipPercentage/100)
 }
+
+app.get('/magic/:question', (request, response) => {
+	response.send(`<p>${request.params.question}</p> <p>${question[number(question.length)]}</p>`)
+
+})
+let number = function(max){
+	return Math.floor(Math.random()*max)
+}
+
+
+
 
 app.listen(9000);
